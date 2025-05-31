f9c6cee1ce5b8d27f676c7734adfbf8450e7615f
Chetan Panara <chetanpanara8@gmail.com>
2025-05-31 12:35:56 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="./img/cpnew.png" atl="logo" />
+        <link rel="icon" href="./img/logo.png" atl="logo" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
